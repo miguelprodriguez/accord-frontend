e00acd9da8b39529260ff0e98a3672fe8609c336
Miguel Rodriguez <mglprodriguez@gmail.com>
2023-06-22 11:32:57 +0800

    Change page name on signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,6 @@
 import FormContainer from "@/components/FormContainer";
 
-export default function Home() {
+export default function Signup() {
   return (
     <div className="flex justify-center items-center h-full px-8">
       <FormContainer />
