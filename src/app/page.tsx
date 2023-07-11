131d63d39706bae61cb0ee037868638f9a9b1fb8
Miguel Rodriguez <mglprodriguez@gmail.com>
2023-07-11 16:14:56 +0800

    Add checking for private route of home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,37 @@
 'use client'
-
 import ChatWindow from "@/components/ChatWindow";
 import Sidebar from "@/components/Sidebar";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState<Boolean | null>(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    const loginUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/users/login`
+    axios.get(loginUrl, { withCredentials: true })
+      .then(() => {
+        setIsUserLoggedIn(true)
+      })
+      .catch(() => {
+        router.push("/login")
+      })
+  }, [])
+
   return (
-    <div className="flex h-full">
-      <Sidebar />
-      <ChatWindow />
-    </div>
+    <>
+      {
+        !isUserLoggedIn
+          ? ''
+          : (
+            <div className="flex h-full">
+              <Sidebar />
+              <ChatWindow />
+            </div>
+          )
+      }
+    </>
   )
 }
