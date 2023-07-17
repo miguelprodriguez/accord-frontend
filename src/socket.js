ed691a67577fe795895edbd1ff02d9d060421fd9
Miguel Rodriguez <mglprodriguez@gmail.com>
2023-07-17 17:06:19 +0800

    Remove URL console log

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,6 @@ import { io } from 'socket.io-client';
 
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
 
-console.log("URL: ", URL)
 export const socket = io(URL, {
     autoConnect: false,
     withCredentials: true
